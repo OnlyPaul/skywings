d3acd1245f5da90454841107a59543e0f2634af3
sebse <s-karner@gmx.at>
2016-12-08 15:52:29 +0100

    controller erstellt

--- model/FlugController.java
+++ model/FlugController.java
@@ -1,46 +1,64 @@
+import package model;
 import interface FlugDAO;
 
 
 public class FlugController  {
  
-	FlugDAO flugdao;
+	FlugDAO flugDAO;
 	
 	
-	public FlugcCntroller(string s){
+	public FlugController(string pfad){
 		
-		flugdao=new FlugDAO s;
+		
+		flugdao = new SerializedFlugDAO(pfad);
 		
 	}
 	
-	public void setFlugDAO(FlugDAO f){
+	public FlugDAO getFlugDAO(){
 		
-		
-	}
-	
-	public getFlugDAO(){ 
-		return flugDAO;
-	
-	}
-	
-	public void flugEintragen(Flug flug){
-		
-		
+		return FlugDAO;
 		
 	}
 	
-	public void flugAendern(flugid id){
-		
-		
+	public void setFlugDAO(FlugDAO flugDAO){
+		this.flugDAO=flugDAO;
 		
 	}
-	public void flugLoeschen(flugid id){
-		Flug f=flugdao
+	
+		
+	public boolean flugEintragen(int flugid, String flugnummer, double preis, Date abDatum, Date anDatum, String abflugsort, String ankunftsort){
+		try{
+			Flug neuflug= new Flug (flugid,flugnummer, preis, abflugsort, ankunftsort, abDatum, anDatum );
+			flugDAO.speichereFlug(neuflug);
+			return true;
+		}
+		catch(IOException e){
+			return false;
+		}
+		
+	}
+	
+	public boolean flugAendern(int flugid, Date neuabDatum, neuanDatum){
+		try{
+			Flug.getFlugid(id).setAbflugsdatum=neuabDatum;
+		
+			Flug.getFLugid(id).setAnkunftsdatum=neuanDatum;
+			return true;
+		}
+		catch(IOException e){
+			return false;
+		}
+					
+	}
+	
+	public void flugLoeschen(int flugid){
+		flugDAO.loescheFlug(flugDAO.getFlugbyId(id));
 		
 	}
 	
 	//klassendiagramm ändern
-	public flugSuche(String fnr){
-		
+	public flugSuche(int flugid){
+		Flug.getFlugid(id).
 	}
 //FLugliste	
 	
