a38787706e454addbf6a0a6442d6a892552f0ba2
sebse <s-karner@gmx.at>
2016-12-08 14:53:49 +0100

    FLugcontrol

---
+++ model/FlugController.java
@@ -0,0 +1,49 @@
+import interface FlugDAO;
+
+
+public class FlugController  {
+ 
+	FlugDAO flugdao;
+	
+	
+	public FlugcCntroller(string s){
+		
+		flugdao=new FlugDAO s;
+		
+	}
+	
+	public void setFlugDAO(FlugDAO f){
+		
+		
+	}
+	
+	public getFlugDAO(){ 
+		return flugDAO;
+	
+	}
+	
+	public void flugEintragen(Flug flug){
+		
+		
+		
+	}
+	
+	public void flugAendern(flugid id){
+		
+		
+		
+	}
+	public void flugLoeschen(flugid id){
+		Flug f=flugdao
+		
+	}
+	
+	//klassendiagramm ändern
+	public flugSuche(String fnr){
+		
+	}
+//FLugliste	
+	
+}
+
+
